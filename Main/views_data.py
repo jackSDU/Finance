0dd39967a23453844cd65d3a47c4db43a7d4a71c
LV QF <LiQuidFly@126.com>
2015-07-10 15:35:02 +0800

    Add pagination support

--- Main/views_data.py
+++ Main/views_data.py
@@ -1,15 +1,31 @@
-from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
-from Main.views import ren2res
+from django.contrib.auth.decorators import login_required
+from django.http import QueryDict
+
+from Main.views import ren2res,paginate
 from Main.models_data import *
 
-from datetime import date
+def render(template,req,qs):
+    q=QueryDict(mutable=True)
+    dict={}
+    sd=req.GET.get('sdate')
+    if sd:
+        q['sdate']=sd
+        dict.update(sdate=sd)
+        sd=datetime.strptime(sd,"%Y-%m-%d")
+        qs=qs.filter(trade_date__gte=sd)
+    ed=req.GET.get('edate')
+    if ed:
+        q['edate']=ed
+        dict.update(edate=ed)
+        ed=datetime.strptime(ed,"%Y-%m-%d")
+        qs=qs.filter(trade_date__lte=ed)
+    dict.update(paginate(req,qs))
+    return ren2res(template,req,dict)
 
 # Create your views here.
 
 def interest_rate(req):
     if req.method=='GET':
-        qs=InterestRate.objects.using('data').all()[:10]
-    #    if req.GET.get('sdate'):
-    #        err=req.GET.get('sdate')
-        return ren2res('data/interest_rate.html',req,{'item':qs,'err':req.GET.get('sdate')})
+        return render('data/interest_rate.html',req,InterestRate.objects.using('data'))
