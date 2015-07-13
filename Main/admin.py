97fca613c8833dc0ca4c39286109de59dab3d83c
LV QF <LiQuidFly@126.com>
2015-07-13 09:50:33 +0800

    Add models to admin

--- Main/admin.py
+++ Main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from Main.models import *
+
 # Register your models here.
+
+admin.site.register(Host)
+admin.site.register(App)
+admin.site.register(Param)
+admin.site.register(Job)
+admin.site.register(JobParam)
