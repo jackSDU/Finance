37e27e85700c2b28653d1fee79fbced8ac25cdaf
LV QF <LiQuidFly@126.com>
2015-07-13 10:42:39 +0800

    Add iv_index,and modify data pages render.

--- Main/views_data.py
+++ Main/views_data.py
@@ -2,30 +2,38 @@ from datetime import datetime
 
 from django.contrib.auth.decorators import login_required
 from django.http import QueryDict
+from django.db.models import F
 
 from Main.views import ren2res,paginate
 from Main.models_data import *
 
-def render(template,req,qs):
+def render(template,req,qs,field='trade_date'):
     q=QueryDict(mutable=True)
     dict={}
+    qs=qs.annotate(the_filter_date=F(field))
     sd=req.GET.get('sdate')
     if sd:
         q['sdate']=sd
         dict.update(sdate=sd)
         sd=datetime.strptime(sd,"%Y-%m-%d")
-        qs=qs.filter(trade_date__gte=sd)
+        qs=qs.filter(the_filter_date__gte=sd)
     ed=req.GET.get('edate')
     if ed:
         q['edate']=ed
         dict.update(edate=ed)
         ed=datetime.strptime(ed,"%Y-%m-%d")
-        qs=qs.filter(trade_date__lte=ed)
+        qs=qs.filter(the_filter_date__lte=ed)
     dict.update(paginate(req,qs))
     return ren2res(template,req,dict)
 
 # Create your views here.
 
+@login_required
 def interest_rate(req):
     if req.method=='GET':
         return render('data/interest_rate.html',req,InterestRate.objects.using('data'))
+
+@login_required
+def iv_index(req):
+    if req.method=='GET':
+        return render('data/iv_index.html',req,IVIndex.objects.using('data'))
