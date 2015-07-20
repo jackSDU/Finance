7db2a9dfa3888eeb45110043653cfc582e054e77
Nich0l4s <sfsixbot@gmail.com>
2015-07-21 01:20:16 +0800

    functions for host management

--- Main/views_apps.py
+++ Main/views_apps.py
@@ -1,3 +1,4 @@
+import ipaddress
 from django.contrib.auth.decorators import login_required
 from django.http import Http404,HttpResponseRedirect
 
@@ -37,6 +38,7 @@ def deploy(req):
         return ren2res("apps/apps_deploy.html", req, {'host': hostlist})
     # request to deploy a new application
     elif req.method == 'POST':
+        print(req.POST)
         # add application info to app table
         info = app_info_check(req)
         if info['err']:
@@ -154,3 +156,84 @@ def app_arg_check(req):
         i += 1
     dict.update(list_submit=list_submit)
     return dict
+
+def host(req):
+    if not req.user.is_superuser:
+            return HttpResponseRedirect('/err/not_admin')
+    hostlist = Host.objects.all()
+    if req.method =='GET':
+        return ren2res("apps/apps_host.html", req, paginate(req, hostlist))
+    elif req.method == 'POST':
+        i = 0
+        page = paginate(req, hostlist)
+        submit = []
+        for n in req.POST.keys():
+            if n[0:6] == 'hostid':
+                host_id = n[6:]
+                try:
+                    item = Host.objects.get(id=host_id)
+                except:
+                    page.update(err="无效的主机ID")
+                    return ren2res("apps/apps_host.html", req, page)
+                name = req.POST['name'+host_id].strip()
+                ip = req.POST['ip'+host_id].strip()
+                port = req.POST['port'+host_id].strip()
+                port = port if port != "" else SERVANT_PORT
+                error = host_check(name, ip, port)
+                if error:
+                    page.update(err=error)
+                    return ren2res("apps/apps_host.html", req, page)
+                item.name = name
+                item.ip = ip
+                item.port = port
+                submit.append(item)
+        for x in submit:
+            x.save()
+        page.update(info='修改成功')
+        return ren2res("apps/apps_host.html", req, page)
+
+def host_add(req):
+    if not req.user.is_superuser:
+        return HttpResponseRedirect('/err/not_admin')
+    if req.method == 'GET':
+        return ren2res("apps/apps_host_add.html",req)
+    elif req.method == 'POST':
+        name = req.POST['host_name']
+        ip = req.POST['host_ip']
+        port = req.POST['host_port']
+        port = port if port != "" else SERVANT_PORT
+        error = host_check(name, ip, port)
+        if error:
+            return ren2res("apps/apps_host_add.html", req, {'err':error})
+        Host(name=name, ip=ip, port=port).save()
+        return ren2res("apps/apps_host_add.html", req, {'info':'主机添加成功'})
+
+def host_delete(req, n):
+    if not req.user.is_superuser:
+        return HttpResponseRedirect("/err/not_admin")
+    if req.method == 'GET':
+        try:
+            item = Host.objects.get(id=n)
+        except:
+            raise Http404()
+        item.delete()
+        hostlist = Host.objects.all()
+        return HttpResponseRedirect("/apps/host/")
+
+def host_check(name, ip, port):
+    err = ""
+    if name == "":
+        err = "主机名称不能为空"
+        return err
+    try:
+        ipaddress.IPv4Address(ip)
+    except ipaddress.AddressValueError:
+        try:
+            ipaddress.IPv6Address(ip)
+        except ipaddress.AddressValueError:
+            err = '无效的IP地址'
+            return err
+    if int(port) not in range(0, 65535):
+        err = '无效的端口号'
+        return err
+    return err
